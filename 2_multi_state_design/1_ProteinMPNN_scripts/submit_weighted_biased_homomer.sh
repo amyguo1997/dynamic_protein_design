7cb0312e71069b84c43be720218cdddcc33a15e2
Amy Guo <amyguo1997@gmail.com>
2024-07-15 20:49:51 -0700

    Update submit_weighted_biased_homomer.sh

--- 2_multi_state_design/1_ProteinMPNN_scripts/submit_weighted_biased_homomer.sh
+++ 2_multi_state_design/1_ProteinMPNN_scripts/submit_weighted_biased_homomer.sh
@@ -30,6 +30,7 @@ chains_to_design="A B"
 pos_neg_chain_list="A,B"
 chain_betas="1.0,1.0"
 
+#The first amino acid in the chain corresponds to 1 and not PDB residues index 
 fixed_positions="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 43 44 45 46 47 48 49 50 52 53 55 56 59 61 63 65 66 68 69 70 71 72 73 74 75 76 77 78 79 80 82 83 84 86 87 88 89 90, 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 43 44 45 46 47 48 49 50 52 53 55 56 59 61 63 65 66 68 69 70 71 72 73 74 75 76 77 78 79 80 82 83 84 86 87 88 89 90"
 
 python ../helper_scripts/parse_multiple_chains.py --input_path=$folder_with_pdbs --output_path=$path_for_parsed_chains
